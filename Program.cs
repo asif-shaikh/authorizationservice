4c3aedfd8e70b7920cd49f5217916e9faf8f567d
Asif Shaikh <30361183+asif-shaikh@users.noreply.github.com>
2024-10-12 14:09:33 +0530

    Modified Program.cs

--- Program.cs
+++ Program.cs
@@ -5,23 +5,33 @@ using OCorpus.AuthorizationService.Contexts;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<OCorpusDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("OCorpusDbContext"))
 );
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "OCorpus API", Version = "v1" });
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "OCorpus.AuthorizationService.xml"));
+});
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
+app.UseRouting();
+app.UseAuthorization();
 
-app.UseHttpsRedirection();
+app.MapControllers();
+
 
 app.Run();
 
